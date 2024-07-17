ce1fd0a74c212279ddebeda6ea282d4657011fe9
jiwonMe <park@jiwon.me>
2024-07-17 22:28:14 +0900

    init

---
+++ src/api/getCommentCount.ts
@@ -0,0 +1,13 @@
+'use server';
+
+export const getCommentCount = async () => {
+  const response = await fetch(`https://m.hankookilbo.com/article/A2021031811480000657/comment?Page=0`);
+  const json = await response.json();
+  const findByOpinionId = async (opinionId: string) => {
+    const comments = json['list'];
+    return comments.find((comment: any) => comment.opinionId === opinionId);
+  };
+
+  const comment = await findByOpinionId('0e9dbb7b-ca7b-4851-8d92-9a570127514d');
+  return comment;
+};
---
+++ src/components/CommentDisplay.tsx
@@ -0,0 +1,19 @@
+'use server'
+import { getCommentCount } from '@/api/getCommentCount';
+import React from 'react'
+
+const CommentDisplay = async () => {
+
+  const comment = await getCommentCount();
+
+  return (
+    <div className='flex flex-col items-center justify-center border border-gray-300 rounded-md p-4'>
+      감자 댓글 좋아요 수
+      <h1 className='text-2xl'>
+       {JSON.stringify(comment.likeCnt)}
+      </h1>
+    </div>
+  )
+}
+
+export default CommentDisplay
