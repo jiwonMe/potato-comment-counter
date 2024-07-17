6e28f66607a3a67e0224731cfe7524e0e5338b19
jiwonMe <park@jiwon.me>
2024-07-17 22:38:14 +0900

    feat: update title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 import { getCommentCount } from "@/api/getCommentCount";
 import CommentDisplay from "@/components/CommentDisplay";
 import { Metadata } from 'next';
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-2xl font-bold mb-4">장감자 댓글 좋아요 수</h1>
+      <div className="text-left text-zinc-200 pl-4 border-l-2 border-zinc-200 mb-4">
+      남자 90년대생 실자냐? ㅋㅋㅋㅋㅋㅋㅋㅋ <br/>
+아버지랑 딸인줄
+      </div>
+      <div className="flex flex-col items-left justify-start gap-4">
         <CommentDisplay />
+        <Link href='https://m.hankookilbo.com/News/Read/A2021031811480000657#comment-parent-container'>
+        <button className='bg-blue-500 text-white p-2 rounded-md'>
+          좋아요 누르러 가기
+        </button>
+      </Link>
       </div>
     </main>
   );
--- src/components/CommentDisplay.tsx
+++ src/components/CommentDisplay.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 const CommentDisplay = async () => {
 
-  const comment = await getCommentCount();
+  const cnt = await getCommentCount();
 
   return (
     <div className='flex flex-col items-center justify-center border border-gray-300 rounded-md p-4'>
       감자 댓글 좋아요 수
       <h1 className='text-2xl'>
-       {JSON.stringify(comment.likeCnt)}
+       {cnt}
       </h1>
     </div>
   )
